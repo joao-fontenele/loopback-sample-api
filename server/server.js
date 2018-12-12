8fd36d4d394ca8217b117804c9953873496ad0ac
João Paulo Fontenele Brito <jpfb@icomp.ufam.edu.br>
2018-12-12 18:43:05 -0400

    feat(lint): use airbnb linter

--- server/server.js
+++ server/server.js
@@ -1,29 +1,25 @@
-'use strict';
+const loopback = require('loopback');
+const boot = require('loopback-boot');
 
-var loopback = require('loopback');
-var boot = require('loopback-boot');
+const app = loopback();
 
-var app = module.exports = loopback();
-
-app.start = function() {
-  // start the web server
-  return app.listen(function() {
-    app.emit('started');
-    var baseUrl = app.get('url').replace(/\/$/, '');
-    console.log('Web server listening at: %s', baseUrl);
-    if (app.get('loopback-component-explorer')) {
-      var explorerPath = app.get('loopback-component-explorer').mountPath;
-      console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
-    }
-  });
-};
+app.start = () => app.listen(() => {
+  app.emit('started');
+  const baseUrl = app.get('url').replace(/\/$/, '');
+  console.log('Web server listening at: %s', baseUrl);
+  if (app.get('loopback-component-explorer')) {
+    const explorerPath = app.get('loopback-component-explorer').mountPath;
+    console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
+  }
+});
 
 // Bootstrap the application, configure models, datasources and middleware.
 // Sub-apps like REST API are mounted via boot scripts.
-boot(app, __dirname, function(err) {
+boot(app, __dirname, (err) => {
   if (err) throw err;
 
   // start the server if `$ node server.js`
-  if (require.main === module)
-    app.start();
+  if (require.main === module) app.start();
 });
+
+module.exports = app;
