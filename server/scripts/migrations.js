fdf7368d3f15a63d14728fad902aff6758adee36
João Paulo Fontenele Brito <jpfb@icomp.ufam.edu.br>
2019-01-01 18:48:04 -0400

    feat(scripts/migrations): improve automigration script
    allow a -f (force) flag to the script. It uses the automigration function for
    the datastore. That drop tables and recreate them. Instead of running alter
    table commands, like in the autoupdate.

--- server/scripts/migrations.js
+++ server/scripts/migrations.js
@@ -6,25 +6,36 @@ const modelConfig = require('../model-config.json');
 
 const DATASOURCE = 'mysql';
 
-function migrate(dataSource = DATASOURCE) {
-  console.log('automigrating tables');
-
+async function migrate({ dataSource = DATASOURCE, force = false }) {
+  console.log('migrating tables');
   const ds = server.dataSources[dataSource];
   const tables = Object.keys(_.omit(modelConfig, ['_meta']));
 
-  ds.autoupdate(tables, (err) => {
-    if (err) {
-      throw err;
-    }
+  let migrationStrategy = ds.autoupdate;
 
+  if (force) {
+    console.log('WARNING: recreating tables');
+    migrationStrategy = ds.automigrate;
+  }
+
+  try {
+    migrationStrategy = migrationStrategy.bind(ds);
+    await migrationStrategy(tables);
     console.log(`Loopback tables [${tables}] migrated in ${ds.adapter.name}`);
-
+  } catch (err) {
+    console.log(err);
+  } finally {
     ds.disconnect();
-  });
+  }
 }
 
 if (require.main === module) {
-  migrate();
+  let force = false;
+  if (process.argv.indexOf('-f') > -1) {
+    force = true;
+  }
+
+  migrate({ force });
 }
 
 module.exports = {
